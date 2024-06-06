cc025026691a5ff33e86d1e18f917ff62bbd32bf
Shirozo <caranzoneilbryan@gmail.com>
2024-06-06 16:47:11 +0800

    Hello world

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ app.get('/download', async (req, res) => {
         });
 
     } catch (error) {
-        console.log(error);
-        res.status(500).send('An unexpected error occurred');
+        res.status(500).send(error);
     }
 });
 
